3adf9d14cada1dc40b446dfa7f05c98553e173ba
Jaime R Calzada <jreniel@gmail.com>
2020-07-02 10:29:09 -0400

    Removed deprecated files

--- geomesh/mesh/__init__.py
+++
@@ -1,6 +0,0 @@
-from geomesh.mesh.mesh import Mesh
-from geomesh.mesh.euclidean_mesh_2d import EuclideanMesh2D
-__all__ = [
-    "Mesh",
-    "EuclideanMesh2D"
-]
--- geomesh/raster/__init__.py
+++
@@ -1,7 +0,0 @@
-from geomesh.raster.raster import Raster
-from geomesh.raster.raster_collection import RasterCollection
-
-__all__ = [
-    "Raster",
-    "RasterCollection"
-]
