d07a378c70599daf4e45655c5309fcab5fa57207
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2021-01-28 16:03:56 -0500

    Fix growing overlap issue in windowing raster

--- geomesh/raster.py
+++ geomesh/raster.py
@@ -1,3 +1,4 @@
+import math
 import hashlib
 import logging
 import multiprocessing
@@ -773,35 +774,19 @@ def get_iter_windows(
         row_off=0,
         col_off=0
 ):
-    h = chunk_size
-    for i in range(
-        int(row_off),
-        int(row_off + height + chunk_size),
-        chunk_size
-            ):
-        if i + h > row_off + height:
-            h = height - i
-            if h <= 0:
-                break
-        w = chunk_size
-        for j in range(
-            int(col_off),
-            int(col_off + width + chunk_size),
-            chunk_size
-                ):
-            if j + w > col_off + width:
-                w = width - j
-                if w <= 0:
-                    break
-            o = overlap
-            while j + w + o > width:
-                o -= 1
-            w += o
-            o = overlap
-            while i + h + o > height:
-                o -= 1
-            h += o
-            yield windows.Window(j, i, w, h)
+    win_h = chunk_size + overlap
+    win_w = chunk_size + overlap
+    n_win_h = math.ceil(height / chunk_size)
+    n_win_w = math.ceil(width / chunk_size)
+    for i in range(n_win_h):
+        for j in range(n_win_w):
+            off_h = i * chunk_size
+            off_w = j * chunk_size
+            h = chunk_size + overlap
+            h = h - (off_h + h) % height if off_h + h > height else h
+            w = chunk_size + overlap
+            w = w - (off_w + w) % width if off_w + w > width else w
+            yield windows.Window(off_w, off_h, w, h)
 
 
 def get_multipolygon_from_axes(ax):
