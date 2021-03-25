4c794ad0e7a7c180698a808b2887df396d905b2f
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2021-03-25 17:15:32 -0400

    Add patch for collector hfun.

--- geomesh/hfun/collector.py
+++ geomesh/hfun/collector.py
@@ -102,6 +102,19 @@ class ConstantValueContourInfoCollector:
 
 
 
+class RefinementPatchInfoCollector:
+
+    def __init__(self):
+        self._patch_info = dict()
+
+    def add(self, patch_defn, **size_info):
+        self._patch_info[patch_defn] = size_info
+
+    def __iter__(self):
+        for defn, info in self._patch_info.items():
+            yield defn, info
+
+
 
 class HfunCollector(BaseHfun):
 
@@ -109,7 +122,7 @@ class HfunCollector(BaseHfun):
             self,
             in_list: Sequence[
                 Union[str, Raster, Mesh, HfunRaster, HfunMesh]],
-            base_mesh: Mesh,
+            base_mesh: Mesh = None,
             hmin: float = None,
             hmax: float = None,
             nprocs: int = None,
@@ -128,11 +141,14 @@ class HfunCollector(BaseHfun):
         self._hfun_list = list()
         # NOTE: Base mesh has to have a crs otherwise HfunMesh throws
         # exception
-        self._base_mesh = HfunMesh(base_mesh)
+        self._base_mesh = None
+        if base_mesh:
+            self._base_mesh = HfunMesh(base_mesh)
         self._contour_info_coll = RefinementContourInfoCollector()
         self._contour_coll = RefinementContourCollector(
                 self._contour_info_coll)
         self._const_val_contour_coll = ConstantValueContourInfoCollector()
+        self._refine_patch_info_coll = RefinementPatchInfoCollector()
 
         self._type_chk(in_list)
 
@@ -147,7 +163,8 @@ class HfunCollector(BaseHfun):
                 hfun = in_item
 
             elif isinstance(in_item, Raster):
-                in_item.clip(self._base_mesh.mesh.get_bbox(crs=in_item.crs))
+                if self._base_mesh:
+                    in_item.clip(self._base_mesh.mesh.get_bbox(crs=in_item.crs))
                 hfun = HfunRaster(in_item, **self._size_info)
 
             elif isinstance(in_item, EuclideanMesh2D):
@@ -156,7 +173,8 @@ class HfunCollector(BaseHfun):
             elif isinstance(in_item, str):
                 if in_item.endswith('.tif'):
                     raster = Raster(in_item)
-                    raster.clip(self._base_mesh.mesh.get_bbox(crs=raster.crs))
+                    if self._base_mesh:
+                        raster.clip(self._base_mesh.mesh.get_bbox(crs=raster.crs))
                     hfun = HfunRaster(raster, **self._size_info)
 
                 elif in_item.endswith(
@@ -247,12 +265,29 @@ class HfunCollector(BaseHfun):
             source_index, contour_defn0, contour_defn1, value)
 
 
-    def add_patch(self, shape):
+    def add_patch(
+            self,
+            shape: Union[MultiPolygon, Polygon] = None,
+            patch_defn: Patch = None,
+            shapefile: Union[None, str, Path] = None,
+            expansion_rate: float = None,
+            target_size: float = None,
+    ):
 
         self._applied = False
 
-        raise NotImplementedError(
-            "Patch is not implemented for collector hfun!")
+        if not patch_defn:
+            if shape:
+                patch_defn = Patch(shape=shape)
+
+            elif shapefile:
+                patch_defn = Patch(shapefile=shapefile)
+
+        self._refine_patch_info_coll.add(
+            patch_defn,
+            expansion_rate=expansion_rate,
+            target_size=target_size)
+
 
     def _type_chk(self, input_list):
         ''' Check the input type for constructor '''
@@ -326,8 +361,18 @@ class HfunCollector(BaseHfun):
 
 
     def _apply_patch(self):
-        raise NotImplementedError(
-            "Patch is not implemented for collector hfun!")
+
+        contourable_list = [
+            i for i in self._hfun_list if isinstance(i, HfunRaster)]
+
+        # TODO: Parallelize
+        for hfun in contourable_list:
+            for patch_defn, size_info in self._refine_patch_info_coll:
+                hfun.add_patch(
+                        patch_defn.get_multipolygon(),
+                        nprocs=self._nprocs,
+                        **size_info)
+
 
     def _write_hfun_to_disk(self, out_path):
 
