18d5b2be18ad6448ba01a7064ea9099feb23f3d1
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2021-05-17 05:53:24 +0000

    Fix for patch CRS and shapefile exist checking

--- geomesh/features/patch.py
+++ geomesh/features/patch.py
@@ -20,23 +20,38 @@ class Patch:
         self._shapefile = Path(shapefile)
         if isinstance(shape, Polygon):
             self._shape = MultiPolygon([shape])
+
         elif isinstance(shape, MultiPolygon):
             self._shape = shape
+
         elif shape != None:
             raise TypeError(
                 f"Type of shape input must be either {MultiPolygon}"
                 f" or {Polygon}")
+            
+        elif not self._shapefile.is_file():
+            raise ValueError(
+                f"Not shape input for patch definition")
+
 
     def get_multipolygon(self) -> MultiPolygon:
 
         if self._shape:
-            return self._shape
+            dst_crs = CRS.from_user_input("EPSG:4326")
+            return self._shape, dst_crs
 
         elif self._shapefile.is_file():
             gdf = gpd.read_file(self._shapefile)
-            dst_crs = CRS.from_user_input("EPSG:4326")
-            if gdf.crs != dst_crs:
-                gdf = gdf.to_crs(dst_crs)
-            multipolygon = MultiPolygon([i for i in gdf.geometry])
+            poly_list = list()
+            for shp in gdf.geometry:
+                if isinstance(shp, Polygon):
+                    poly_list.append(shp)
+                elif isinstance(shp, MultiPolygon):
+                    poly_list.extend([pl for pl in shp.geoms])
+
+            multipolygon = MultiPolygon(poly_list)
             
-            return multipolygon, dst_crs
+            return multipolygon, gdf.crs
+
+        raise ValueError(
+            f"Error retrieving shape information for patch")
