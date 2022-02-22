dbf26458e939714d9147a184df5e693e18d98e2a
SorooshMani-NOAA <SorooshMani-NOAA@users.noreply.github.com>
2022-02-22 22:10:25 +0000

    Deploying to gh-pages from @ noaa-ocs-modeling/OCSMesh@8329794913370284d0d8b26b8157f89688ddb3eb 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post1.dev0+8329794',
+    VERSION: '1.0.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
