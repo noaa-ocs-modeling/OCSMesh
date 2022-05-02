0ffb937589af8b631e5fc192966377a667305e71
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2022-05-02 13:03:02 -0400

    Add loglevel argument for script CLI

--- ocsmesh/cli/cli.py
+++ ocsmesh/cli/cli.py
@@ -1,3 +1,4 @@
+import logging
 import warnings
 
 from ocsmesh.cli.remesh_by_shape_factor import RemeshByShape
@@ -11,6 +12,7 @@ class CmdCli:
         # TODO: Later add non experimental CLI through this class
         self._script_dict = {}
 
+        parser.add_argument("--loglevel")
         scripts_subp = parser.add_subparsers(dest='scripts_cmd')
         for cls in [RemeshByShape, RemeshByDEM, MeshUpgrader]:
             item = cls(scripts_subp)
@@ -22,4 +24,7 @@ class CmdCli:
             "Scripts CLI is used for experimental new features"
             " and is subject to change.")
 
+        if args.loglevel is not None:
+            logging.getLogger().setLevel(
+                    getattr(logging, args.loglevel.upper()))
         self._script_dict[args.scripts_cmd].run(args)
