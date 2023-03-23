75f1d870318a5717719f275c157a53e8fa991e03
Soroosh.Mani <Soroosh.Mani@noaa.gov>
2023-03-23 12:59:46 -0400

    Fix local testing with unittest

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -1,1 +1,7 @@
-
+import os
+os.system("""
+    ls /tmp/test_dem.tif > /dev/null
+    if [ $? -eq 0 ]; then exit; fi
+    wget https://coast.noaa.gov/htdata/raster2/elevation/NCEI_ninth_Topobathy_2014_8483/northeast_sandy/ncei19_n40x75_w073x75_2015v1.tif -O /tmp/fullsize_dem.tif
+    gdalwarp -tr 0.0005 0.0005 -r average -overwrite /tmp/fullsize_dem.tif /tmp/test_dem.tif
+""")
