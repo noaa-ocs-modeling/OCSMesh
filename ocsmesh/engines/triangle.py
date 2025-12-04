b48dafe0d8fe64e2a7435d9d5deb028895c3f199
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2025-12-04 12:56:55 -0500

    Reorganize file

--- ocsmesh/engines/triangle.py
+++ ocsmesh/engines/triangle.py
@@ -20,152 +20,6 @@ from ocsmesh.engines.base import BaseMeshEngine, BaseMeshOptions
 _logger = logging.getLogger(__name__)
 
 
-def shape_to_triangle_dict(
-    shape: Union[Polygon, MultiPolygon]
-) -> Dict[str, Any]:
-    """
-    Convert a Shapely polygon to Triangle library input dict.
-    """
-    if isinstance(shape, Polygon):
-        shape = MultiPolygon([shape])
-
-    if not isinstance(shape, MultiPolygon):
-        raise ValueError("Input must be Polygon or MultiPolygon")
-
-    gs_shape = gpd.GeoSeries(shape)
-
-    if np.sum(gs_shape.area) == 0:
-        raise ValueError("The shape must have an area, such as Polygon!")
-
-    if not np.all(gs_shape.is_valid):
-        raise ValueError("Input contains invalid (multi)polygons!")
-
-    df_lonlat = (
-        gs_shape
-        .boundary
-        .explode(ignore_index=True)
-        .map(lambda i: i.coords)
-        .explode()
-        .apply(lambda s: pd.Series({'lon': s[0], 'lat': s[1]}))
-        .reset_index() # put polygon index in the dataframe
-        .drop_duplicates() # drop duplicates within polygons
-    )
-
-    df_seg = (
-        df_lonlat.join(
-            df_lonlat.groupby('index').transform(np.roll, 1, axis=0),
-            lsuffix='_1',
-            rsuffix='_2'
-        ).dropna()
-        .set_index('index')
-    )
-
-    df_nodes = (
-        df_lonlat.drop(columns='index') # drop to allow cross poly dupl
-        .drop_duplicates() # drop duplicates across multiple polygons
-        .reset_index(drop=True) # renumber nodes
-        .reset_index() # add node idx as df data column
-        .set_index(['lon','lat'])
-    )
-
-    # CRD Table
-    df_coo = df_nodes.reset_index().drop(columns='index')
-
-    # CNN Table
-    df_edg = (
-        df_nodes.loc[
-            pd.MultiIndex.from_frame(df_seg[['lon_1', 'lat_1']])
-        ].reset_index(drop=True)
-        .join(
-            df_nodes.loc[
-                pd.MultiIndex.from_frame(df_seg[['lon_2', 'lat_2']])
-            ]
-            .reset_index(drop=True),
-            lsuffix='_1',
-            rsuffix='_2'
-        )
-    )
-
-
-    ar_edg = np.sort(df_edg.values, axis=1)
-    df_cnn = (
-        pd.DataFrame.from_records(
-            ar_edg,
-            columns=['index_1', 'index_2'],
-        )
-        .drop_duplicates() # Remove duplicate edges
-        .reset_index(drop=True)
-    )
-
-    vertices = df_coo[['lon', 'lat']].values
-    segments = df_cnn.values
-
-
-    # To make sure islands formed by two polygons touching at multiple
-    # points are also considered as holes, instead of getting interiors,
-    # we get the negative of the domain and calculate points in all
-    # negative polygons!
-
-    # buffer by 1/100 of shape length scale = sqrt(area)/100
-    holes = []
-    world = box(*shape.buffer(np.sqrt(shape.area) / 100).bounds)
-    neg_shape = world.difference(shape)
-    if isinstance(neg_shape, Polygon):
-        neg_shape = MultiPolygon([neg_shape])
-
-    for poly in neg_shape.geoms:
-        holes.append(np.array(poly.representative_point().xy).ravel())
-
-    data = {
-        'vertices': np.array(vertices, dtype=float),
-        'segments': np.array(segments, dtype=int)
-    }
-    if holes:
-        data['holes'] = np.array(holes, dtype=float)
-
-    return data
-
-
-def meshdata_to_triangle_dict(mesh: MeshData) -> Dict[str, Any]:
-    """
-    Convert MeshData to Triangle library input dict.
-    """
-    data = {}
-    if mesh.coords is not None:
-        data['vertices'] = mesh.coords
-
-    if mesh.tria is not None and mesh.tria.size > 0:
-        data['triangles'] = mesh.tria
-
-    # If simple segments are needed (e.g. boundary), they must be
-    # calculated. For simple remeshing of an existing mesh,
-    # 'vertices' and 'triangles' are often enough for 'r' mode.
-    # If using 'p' mode on existing nodes, segments are needed.
-
-    # We can infer boundary segments if not provided, but
-    # Triangle library usually expects PSLG for 'p'.
-
-    return data
-
-
-def triangle_dict_to_meshdata(
-    data: Dict[str, Any],
-) -> MeshData:
-    """
-    Convert Triangle library output dict to MeshData.
-    """
-    coords = data.get('vertices')
-    tria = data.get('triangles')
-
-    # Triangle outputs segments/edges too, usually we ignore
-    # unless we want to store them.
-
-    return MeshData(
-        coords=coords,
-        tria=tria,
-    )
-
-
 class TriangleOptions(BaseMeshOptions):
     """
     Wraps options for the Triangle library.
@@ -328,3 +182,149 @@ class TriangleEngine(BaseMeshEngine):
         out_dict = tr.triangulate(input_dict, opts)
 
         return triangle_dict_to_meshdata(out_dict)
+
+
+def shape_to_triangle_dict(
+    shape: Union[Polygon, MultiPolygon]
+) -> Dict[str, Any]:
+    """
+    Convert a Shapely polygon to Triangle library input dict.
+    """
+    if isinstance(shape, Polygon):
+        shape = MultiPolygon([shape])
+
+    if not isinstance(shape, MultiPolygon):
+        raise ValueError("Input must be Polygon or MultiPolygon")
+
+    gs_shape = gpd.GeoSeries(shape)
+
+    if np.sum(gs_shape.area) == 0:
+        raise ValueError("The shape must have an area, such as Polygon!")
+
+    if not np.all(gs_shape.is_valid):
+        raise ValueError("Input contains invalid (multi)polygons!")
+
+    df_lonlat = (
+        gs_shape
+        .boundary
+        .explode(ignore_index=True)
+        .map(lambda i: i.coords)
+        .explode()
+        .apply(lambda s: pd.Series({'lon': s[0], 'lat': s[1]}))
+        .reset_index() # put polygon index in the dataframe
+        .drop_duplicates() # drop duplicates within polygons
+    )
+
+    df_seg = (
+        df_lonlat.join(
+            df_lonlat.groupby('index').transform(np.roll, 1, axis=0),
+            lsuffix='_1',
+            rsuffix='_2'
+        ).dropna()
+        .set_index('index')
+    )
+
+    df_nodes = (
+        df_lonlat.drop(columns='index') # drop to allow cross poly dupl
+        .drop_duplicates() # drop duplicates across multiple polygons
+        .reset_index(drop=True) # renumber nodes
+        .reset_index() # add node idx as df data column
+        .set_index(['lon','lat'])
+    )
+
+    # CRD Table
+    df_coo = df_nodes.reset_index().drop(columns='index')
+
+    # CNN Table
+    df_edg = (
+        df_nodes.loc[
+            pd.MultiIndex.from_frame(df_seg[['lon_1', 'lat_1']])
+        ].reset_index(drop=True)
+        .join(
+            df_nodes.loc[
+                pd.MultiIndex.from_frame(df_seg[['lon_2', 'lat_2']])
+            ]
+            .reset_index(drop=True),
+            lsuffix='_1',
+            rsuffix='_2'
+        )
+    )
+
+
+    ar_edg = np.sort(df_edg.values, axis=1)
+    df_cnn = (
+        pd.DataFrame.from_records(
+            ar_edg,
+            columns=['index_1', 'index_2'],
+        )
+        .drop_duplicates() # Remove duplicate edges
+        .reset_index(drop=True)
+    )
+
+    vertices = df_coo[['lon', 'lat']].values
+    segments = df_cnn.values
+
+
+    # To make sure islands formed by two polygons touching at multiple
+    # points are also considered as holes, instead of getting interiors,
+    # we get the negative of the domain and calculate points in all
+    # negative polygons!
+
+    # buffer by 1/100 of shape length scale = sqrt(area)/100
+    holes = []
+    world = box(*shape.buffer(np.sqrt(shape.area) / 100).bounds)
+    neg_shape = world.difference(shape)
+    if isinstance(neg_shape, Polygon):
+        neg_shape = MultiPolygon([neg_shape])
+
+    for poly in neg_shape.geoms:
+        holes.append(np.array(poly.representative_point().xy).ravel())
+
+    data = {
+        'vertices': np.array(vertices, dtype=float),
+        'segments': np.array(segments, dtype=int)
+    }
+    if holes:
+        data['holes'] = np.array(holes, dtype=float)
+
+    return data
+
+
+def meshdata_to_triangle_dict(mesh: MeshData) -> Dict[str, Any]:
+    """
+    Convert MeshData to Triangle library input dict.
+    """
+    data = {}
+    if mesh.coords is not None:
+        data['vertices'] = mesh.coords
+
+    if mesh.tria is not None and mesh.tria.size > 0:
+        data['triangles'] = mesh.tria
+
+    # If simple segments are needed (e.g. boundary), they must be
+    # calculated. For simple remeshing of an existing mesh,
+    # 'vertices' and 'triangles' are often enough for 'r' mode.
+    # If using 'p' mode on existing nodes, segments are needed.
+
+    # We can infer boundary segments if not provided, but
+    # Triangle library usually expects PSLG for 'p'.
+
+    return data
+
+
+def triangle_dict_to_meshdata(
+    data: Dict[str, Any],
+) -> MeshData:
+    """
+    Convert Triangle library output dict to MeshData.
+    """
+    coords = data.get('vertices')
+    tria = data.get('triangles')
+
+    # Triangle outputs segments/edges too, usually we ignore
+    # unless we want to store them.
+
+    return MeshData(
+        coords=coords,
+        tria=tria,
+    )
